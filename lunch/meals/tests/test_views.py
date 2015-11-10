cb50ddccf25160d892c3418955ef5deaef7d6e65
Balint Vekerdy <balint@dangerfarms.com>
2015-11-10 09:14:49 +0100

    Finish today meal view

--- lunch/meals/tests/test_views.py
+++ lunch/meals/tests/test_views.py
@@ -1,6 +1,5 @@
 import datetime
 from unittest import mock, skip
-
 from lunch.cards.models import Card
 from lunch.meals.factories import MealFactory
 from lunch.meals.models import Meal
@@ -18,10 +17,9 @@ class TodayMealByUUIDTest(APITestCase):
     def setUp(self):
         Card.objects.all().delete()
         Profile.objects.all().delete()
+        Meal.objects.all().delete()
 
-    @mock.patch('lunch.meals.views.timezone')
-    def test_should_return_the_meal_option_for_the_person_with_uuid_for_today(self, mock_timezone):
-        mock_timezone.now.return_value = datetime.datetime(2015, 11, 9, 0, 0)
+    def test_should_return_the_meal_option_for_the_person_with_uuid_for_today(self):
         profile = ProfileFactory(remote_system_id=1, full_name='Test Person')
         card = profile.card_set.first()
         MealFactory(profile=profile, meal_type=Meal.MEAL_A)
@@ -31,8 +29,22 @@ class TodayMealByUUIDTest(APITestCase):
         self.assertEqual(response.data['available'], True)
         self.assertEqual(response.data['full_name'], 'Test Person')
 
-    @skip('implement')
     @mock.patch('lunch.meals.views.timezone')
     def test_should_return_todays_meal(self, mock_timezone):
-        mock_timezone.now.return_value = datetime.datetime(2015, 11, 9, 0, 0)
-        # create two meals, one for today, one for tomorrow
+        now = datetime.datetime(2015, 11, 9, 0, 0)
+        mock_timezone.now.return_value = now
+        profile = ProfileFactory(remote_system_id=1, full_name='Test Person')
+        card = profile.card_set.first()
+        MealFactory(profile=profile, meal_type=Meal.MEAL_A, date=now.date())
+        MealFactory(profile=profile, meal_type=Meal.MEAL_B, date=(now.date() - datetime.timedelta(days=1)))
+        response = self.client.get(reverse(self.url_name, kwargs={'uuid': card.uuid}))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['meal_type'], 'A')
+
+    def test_should_return_404_if_no_meal_for_person(self):
+        profile = ProfileFactory(remote_system_id=1, full_name='Test Person')
+        other_profile = ProfileFactory(remote_system_id=2, full_name='Test Person 2')
+        other_persons_card = other_profile.card_set.first()
+        MealFactory(profile=profile, meal_type=Meal.MEAL_A)
+        response = self.client.get(reverse(self.url_name, kwargs={'uuid': other_persons_card.uuid}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
--- lunch/meals/views.py
+++ lunch/meals/views.py
@@ -2,20 +2,23 @@ from django.utils import timezone
 from lunch.cards.models import Card
 from lunch.meals.models import Meal
 from lunch.meals.serializers import MealSerializer
-from rest_framework.generics import RetrieveAPIView
+from rest_framework.generics import RetrieveAPIView, get_object_or_404
 
 
 class TodayMealByUUID(RetrieveAPIView):
     URL_NAME = 'profile-today-meal'
 
-    queryset = Meal.objects.filter(available=True, date=timezone.now())
+    queryset = Meal.objects.filter(available=True)
     serializer_class = MealSerializer
 
     class Meta:
         model = Meal
 
     def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
         uuid = self.kwargs.pop('uuid')
         card = Card.objects.get(uuid=uuid)
-        meal = self.queryset.filter(profile=card.profile).first()
-        return meal
+        obj = get_object_or_404(queryset, profile=card.profile, date=timezone.now())
+        # May raise a permission denied
+        self.check_object_permissions(self.request, obj)
+        return obj
